02ac09dd8ffbd603d00809cdd36d0d1d054e1e14
Maksim Boydenko <boidenko.maxim@gmail.com>
2025-12-01 20:24:45 +0300

    Refactoring

--- acteventtick/__init__.py
+++ acteventtick/__init__.py
@@ -1,4 +1,4 @@
 from acteventtick.events.event import Event
 from acteventtick.actions.action import Action
 from acteventtick.actions.action_handler import ActionHandler
-from acteventtick.loop import ActEvenTickLoop
+from acteventtick.loop import ActEventTickLoop
